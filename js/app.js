f9c98103ad97f9d9eceae36760895a49a7057f37
Santi Scalzadonna <santiago.scalzadonna@youbim.com>
2016-05-19 16:13:03 -0300

    last muppets

--- js/app.js
+++ js/app.js
@@ -21,4 +21,38 @@ var muppets = new MuppetsCollection();
 
 muppets.fetch().then(function() {
     console.log(muppets.length); // >> length: 1
+    _.each(muppets.models, function(mup){console.log(mup.get('name'))});
 });
+
+
+var KermitModel = Backbone.Model.extend({
+    url: 'http://demo2352365.mockable.io/muppets/1',
+    defaults: {
+        name: '',
+        occupation: ''
+    }
+});
+
+var KermitView = Backbone.View.extend({
+    el: '#muppets',
+
+    initialize: function() {
+
+        this.model.fetch();
+        this.render();
+    },
+
+    render: function() {
+        var html = '<b>Name:</b> ' + this.model.get('name');
+        html += ', occupation: ' + this.model.get('occupation');
+        this.$el.html(html);
+        return this;
+    }
+});
+
+var kermit = new KermitModel();
+var kermitView = new KermitView({model: kermit});
+
+
+
+
